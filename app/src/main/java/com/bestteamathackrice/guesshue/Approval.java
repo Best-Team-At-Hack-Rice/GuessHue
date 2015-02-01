f180de6f141ee45415c4c7d491b4af2295a82808
Greg Kinman <gregakinman@gmail.com>
2015-01-31 18:10:41 -0600

    Working on getting full-size photos.

--- app/src/main/java/com/bestteamathackrice/guesshue/Approval.java
+++ app/src/main/java/com/bestteamathackrice/guesshue/Approval.java
@@ -2,6 +2,7 @@ package com.bestteamathackrice.guesshue;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
@@ -59,17 +60,34 @@ public class Approval extends ActionBarActivity {
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+            }
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-
+    // TODO: DELETE IMAGE AFTER DONE TO SAVE SPACE
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView image = (ImageView) findViewById(R.id.user_color);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            image.setImageBitmap(imageBitmap);
+            Uri imageUri = data.getData();
+            try {
+                Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
+                        imageUri);
+                image.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            image.setImageBitmap(imageBitmap);
         }
     }
 
@@ -77,8 +95,8 @@ public class Approval extends ActionBarActivity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
+        // TODO: is this the right argument for this method?
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
